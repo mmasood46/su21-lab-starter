614b8567b3aac020b0ef79790d360d1e3a999eac
muhammad masood <mmasudaiou@gmail.com>
2024-03-01 05:33:24 +0500

    exercise4 done

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -3,4 +3,23 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    
+// Initialized the pointers: fast_ptr and slow_ptr 
+node *fast_ptr = head;
+node *slow_ptr = head;
+// to traverse the list with two pointers 
+while (fast_ptr != NULL && fast_ptr->next != NULL) {
+// fast_ptr advances by two nodes 
+fast_ptr = fast_ptr->next->next;
+//  slow_ptr advances by one node 
+slow_ptr = slow_ptr->next;
+// Check: whether fast_ptr and slow_ptr pointing the same node 
+if (fast_ptr == slow_ptr) {
+//If they point to the same node, the list is having cycle
+return 1;
+}
+}
+//If fast_ptr reache the end of the list (NULL), the list is acyclic 
+return 0;
+
 }
